7daa799160c39239aa4badc1904c9e1c18c1e883
YuvrajPatidar12 <yuvraj.patidar@codiatic.com>
2024-01-08 15:44:26 +0530

    8 jan 2nd time commit

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,5 +1,5 @@
 class Hotel < ApplicationRecord
   has_and_belongs_to_many :users, dependent: :destroy
   has_many :services, dependent: :destroy
-  has_many :rooms
+  has_many :rooms, dependent: :destroy
 end
