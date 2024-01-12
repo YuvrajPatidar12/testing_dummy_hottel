1c5196baae61682dbfd254c13e5de5046de0d372
YuvrajPatidar12 <154416372+YuvrajPatidar12@users.noreply.github.com>
2024-01-12 15:01:22 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,8 +81,8 @@ Rails.application.configure do
   # config.action_mailer.smtp_settings = {
   #                                       :address => 'smtp.gmail.com',
   #                                       :port => 587,
-  #                                       :user_name => "patidaryuvraj504@gmail.com",
-  #                                       :password => "eqrnzxyvbnlhscsf",
+  #                                       :user_name => "###############",
+  #                                       :password => "*****************",
   #                                       :authentication => 'plain',
   #                                       :enable_starttls_auto => true
   #                                       }
